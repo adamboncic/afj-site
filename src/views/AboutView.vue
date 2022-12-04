91c00a3516780e5cefddb3efed450ffbea9ee991
Adam Boncic <adam.boncic@gramercytech.com>
2022-12-04 18:30:56 -0500

    bio update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,7 +4,7 @@
       <div class="about-desc col-sm-4 order-2 shift-in" v-cloak>
           <h3 class="page-title">About Me</h3>
           <p>
-            Annika Chavez is a director, writer, and actor. She is a graduate of the University of Southern California where she studied theatre and film and is currently based between Los Angeles and New York. She has 9 years of directing experience with theatre as her base. Her 2021 film BOLOGNESE recently played at both Palm Springs International Shortfest, where it was nominated for best US Short, and at NFFTY, where it won an Audience Award. It continues to make festival rounds this year. She is currently in post-production for two short films, HELIUM and BAD JEW GOOD GIRL, that she directed this year. As a writer, she was a semifinalist for the 2021 NBC Nosotros Monologue Slam.
+            Annika Chavez is a director, writer, and actor. She is a graduate of the University of Southern California where she studied theatre and film and is currently based between Los Angeles and New York. She has 9 years of directing experience with theatre as her base. Her 2021 film BOLOGNESE recently played at both Palm Springs International Shortfest, where it was nominated for best US Short, and at NFFTY, where it won an Audience Award, among others. It continues to make festival rounds this year. Her short, HELIUM was just completed and is out on the festival circuit as well. She is currently in post-production for BAD JEW GOOD GIRL(short film), that she directed this year. As a writer, she was a semifinalist for the 2021 NBC Nosotros Monologue Slam.
           </p>
         </div>
         <div class="about-img col-sm-6 order-1 order-md-12 shift-in">
